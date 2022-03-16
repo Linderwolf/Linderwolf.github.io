9f56d592f4845b02d4af2295d409fd4075c39676
Linderwolf <91448628+Linderwolf@users.noreply.github.com>
2022-03-16 17:44:05 -0400

    Quick test of iframes

---
+++ index.js
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en" class="h-100">
+  
+  <iframe src="header.html" onload="this.before((this.contentDocument.body||this.contentDocument).children[0]);this.remove()"></iframe>
+  
+  <main class="px-3">
+    <h1>A Webpage for Project Archives:</h1>
+    <p class="lead">by Peter Wolfgang Linder</p>
+	<p>Using a Bootstrap template & hosted by Github Pages</p>
+	<hr>
+    <p class="lead">
+      <a href="MODELab2.html" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Most Recent Lab</a>
+    </p>
+  </main>
+
+  <iframe src="footer.html" onload="this.before((this.contentDocument.body||this.contentDocument).children[0]);this.remove()"></iframe>
+    
+  </body>
+</html>
